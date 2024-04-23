14e67396730819542601eab539b698c63c081e98
DragonChen <teacher144123@gmail.com>
2024-04-23 20:14:25 +0800

    replace soup with colly

---
+++ README.md
@@ -0,0 +1,23 @@
+# Notify Bot
+
+A collection of bots that send notification for myself. The task is mostly composed with web crawler, data pre-procsssing, number calculation.
+
+## Functions
+### Utility
+- [ ] General class of crawling
+- [ ] Dynamic run different scripts
+- [ ] Send notification through different way
+- [ ] Enable generating static report by CI/CD
+
+### Crawler
+- [ ] Crawl web data
+- [ ] Save data locally or through db (e.g. MongoDB, FireBase)
+- [ ] Routinely collect historical data
+
+### Notification
+- [ ] Send data to APP
+- [ ] Chatbot. Get command back from APP
+
+### Else
+- [ ] Virtualization (e.g. docker, k8s)
+- [ ] No code smells
--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -1,25 +1,15 @@
 package fetch
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/anaskhan96/soup"
-)
-
 // Author:	DragonChen https://github.com/dragonchen-tw/
 // Title:	Package - fetch
 // Date:	2024/04/20
 
 func Fetch() {
-	url := "https://dragonchen.tw/links/"
-	res, err := soup.Get(url)
-	if err != nil {
-		os.Exit(1)
-	}
-	doc := soup.HTMLParse(res)
-	links := doc.FindAll("a")
-	for _, link := range links {
-		fmt.Println(link.Text(), "| Link :", link.Attrs()["href"])
-	}
+	// url := "https://dragonchen.tw/links/"
+	// url := "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=ETH,USD"
+
+	// // JSON
+	// var data map[string]float32
+	// json.Unmarshal([]byte(res), &data)
+	// fmt.Println("ETH price", data)
 }
